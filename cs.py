88f5da053dde1906599496dcb5d03330c0004847
Andrey Antukh <niwi@niwi.be>
2013-11-26 10:00:17 +0100

    Minor changes on the basic compiller.

--- cs.py
+++ cs.py
@@ -9,56 +9,61 @@ import pprint
 
 # TODO: introduce argparse for commandline
 
+# Global class registry
+
 nodes = {}
 
-def _resolve_node(node):
-    return nodes[node.__class__.__name__]
-
-def _resolve_node_instance(node):
-    return _resolve_node(node)(node)
-
+def _resolve_node_instance(node, indent=0):
+    cls = nodes[node.__class__.__name__]
+    return cls(node, indent=indent)
 
 class NodeMeta(type):
-    def __new__(clst, name, bases, attrs):
-        cls = super().__new__(clst, name, bases, attrs)
+    def __init__(cls, name, bases, attrs):
         nodes[name] = cls
-        return cls
+        super().__init__(name, bases, attrs)
 
+
+# Nodes definition
 
 class Node(metaclass=NodeMeta):
     """
     Base class for all compiller Nodes
     """
-    def __init__(self, ast_node, child=None):
+    def __init__(self, ast_node, indent=0, child=None):
         self.ast_node = ast_node
+        self.indent = indent
 
     def iter_nodes(self):
         for node in ast.iter_child_nodes(self.ast_node):
-            yield _resolve_node_instance(node)
+            yield _resolve_node_instance(node, self.indent)
 
-    def get_code(self, indent=0):
+    def get_code(self):
         raise NotImplementedError()
 
 
 class Module(Node):
-    def get_code(self, indent=0):
-        code = "\n\n".join(n.get_code(indent=indent+4) for n in self.iter_nodes())
-        return "".join([";(function() {\n", code, "}).call(this);"])
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.indent += 4
+
+    def get_code(self):
+        code = "\n\n".join(n.get_code() for n in self.iter_nodes())
+        return "".join([";(function(this) {\n", code, "}).call(this);"])
 
 
 class Expr(Node):
-    def get_code(self, indent=0):
+    def get_code(self):
         code = " ".join(x.get_code() for x in self.iter_nodes())
-        return "{indent}{code};\n".format(indent=" "*indent,
+        return "{indent}{code};\n".format(indent=" "*self.indent,
                                           code=code)
 
 class Num(Node):
-    def get_code(self, indent=0):
+    def get_code(self):
         return str(self.ast_node.n)
 
 
 class Add(Node):
-    def get_code(self, indent=0):
+    def get_code(self):
         return "+"
 
 
@@ -78,7 +83,7 @@ class Div(Node):
 
 
 class BinOp(Node):
-    def get_code(self, indent=0):
+    def get_code(self):
         left = _resolve_node_instance(self.ast_node.left)
         op = _resolve_node_instance(self.ast_node.op)
         right = _resolve_node_instance(self.ast_node.right)
@@ -88,25 +93,25 @@ class BinOp(Node):
                                             op=op.get_code())
 
 class Return(Node):
-    def get_code(self, indent=0):
+    def get_code(self):
         code = " ".join(x.get_code() for x in self.iter_nodes())
-        return "{indent}return {code};\n".format(indent=" "*indent,
-                                          code=code)
+        return "{indent}return {code};\n".format(indent=" " * self.indent,
+                                                 code=code)
 
 class FunctionDef(Node):
     def iter_nodes(self):
         for node in self.ast_node.body:
-            yield _resolve_node_instance(node)
+            yield _resolve_node_instance(node, self.indent+4)
 
-    def get_code(self, indent=0):
-        code = ["{0}var {1} = function ()".format(" "*indent, self.ast_node.name)]
+    def get_code(self):
+        code = ["{0}var {1} = function ()".format(" "*self.indent, self.ast_node.name)]
         code.append(" {\n")
 
         for node in self.iter_nodes():
-            code.append(node.get_code(indent=indent+4))
+            code.append(node.get_code())
             code.append("\n")
 
-        code.append(" "*indent)
+        code.append(" "*self.indent)
         code.append("};\n")
         return "".join(code)
 
