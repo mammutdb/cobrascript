81ca4a65fd9f3b25bc84fb0c8fc013683e5f4134
Andrey Antukh <niwi@niwi.be>
2013-12-18 16:08:23 +0100

    Add initial setup.py file.

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='cobrascript',
+      description='Python syntax translator to Javascript.',
+      long_description='Python syntax translator to Javascript.',
+      version='0.1.0',
+      url='https://github.com/niwibe/cobra-script',
+      license='BSD License',
+      platforms=['unix', 'linux', 'osx', 'cygwin', 'win32'],
+      author='Andrey Antukh',
+      author_email='niwi@niwi.be',
+      entry_points={"console_scripts": ["cobrascript = cobra.base:main"]},
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Environment :: Console',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: POSIX',
+                   'Operating System :: Microsoft :: Windows',
+                   'Operating System :: MacOS :: MacOS X',
+                   'Topic :: Software Development :: Libraries',
+                   'Topic :: Utilities',
+                   'Programming Language :: Python :: 3'
+                   'Programming Language :: Python :: 3.3'],
+      install_requires=[],
+      packages=['cobra'],
+      zip_safe=False)
